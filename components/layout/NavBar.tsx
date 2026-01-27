a57e47753750c015c2ba21f4275dffdb8f424e8f
reese-pittman <reeseaverypittman@gmail.com>
2026-01-27 16:33:03 -0600

    I hate git

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,19 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <div>
+                <img src="" alt="" />
+                <link rel="stylesheet" href="" />
+            </div>  
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+            <div>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </nav>
+    )
+}
