4ac1051edeee0de260356df0a043ffb96a84cecb
reese-pittman <reeseaverypittman@gmail.com>
2026-01-27 16:23:09 -0600

    components folder created

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,16 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <div>
+                <img src="" alt="" />
+                <link rel="stylesheet" href="" />
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </nav>
+    )
+}
