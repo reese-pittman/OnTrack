c8a6149a4aadd46c2216c4633a9f00db8f1b968d
reese-pittman <reeseaverypittman@gmail.com>
2026-02-01 00:32:56 -0600

    Landing NavBar UI

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,47 +1,44 @@
 export default function NavBar() {
     return (
-        <nav className="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10 w-screen h-15">
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-between">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+        <nav className="relative bg-gray-50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+            <div className="mr-4 sm:mr-6 lg:mr-8 max-w-screen px-3 sm:pr-6 lg:pr-8">
+                <div className="relative flex h-16 items-center justify-between gap-8">
 
+                    <div className="inset-y-0 left-0 flex items-center justify-start">
                         <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-                            <span className="absolute -inset-0.5"></span>
-                            <span className="sr-only">Open main menu</span>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 in-aria-expanded:hidden">
-                                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6 not-in-aria-expanded:hidden">
-                                <path d="M6 18 18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
+                            <img src="./public/logo.png" alt="OnTrack" className="h-8 w-auto" />
                         </button>
                     </div>
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex shrink-0 items-center">
-                            <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" className="h-8 w-auto" />
-                        </div>
-                        <div className="hidden sm:ml-6 sm:block">
+
+
+                    <div className="flex flex-1 items-center justify-center pl-30"> {/* <Need to figure out how to make these always centered im cheating using paddings> */}
+                        
+                        <div className="sm:ml-6">
                             <div className="flex space-x-4">
 
-                                
-                                <a href="#" aria-current="page" className="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
-                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900">Features</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900">Pricing</a>
+                                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-700 hover:text-gray-900">About</a>
+
                             </div>
                         </div>
                     </div>
-                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button type="button" className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+
+
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0 justify-between space-x-4 gap-4">
+                        <a href="#" className="relative bg-[#e6fef9] rounded-md border-transparent hover:bg-[#ccfef4] shadow-xs font-medium leading-5 rounded-base text-sm px-5 py-2.5">
                             <span className="absolute -inset-1.5"></span>
                             <span className="sr-only">View notifications</span>
-                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" data-slot="icon" aria-hidden="true" className="size-6">
-                                <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" strokeLinecap="round" strokeLinejoin="round" />
-                            </svg>
-                        </button>
 
-                        
-                        
+                            Login
+                        </a>
+
+                        <a href="#" className="relative bg-[#4dfbd7] rounded-md border-transparent hover:bg-[#1afacc] shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5">
+                            <span className="absolute -inset-1.5"></span>
+                            <span className="sr-only">View notifications</span>
+
+                            Get Started
+                        </a>
                     </div>
                 </div>
             </div>
